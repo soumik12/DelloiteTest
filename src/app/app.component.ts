e1aad5a5759d547301bef8110bf7600c5ca2631d
soumik270 <soumik.maiti@gmail.com>
2020-08-29 19:33:47 +0530

    codebase commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from  "@angular/common/http";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'furnitureshop';
+  furnitureArr:any = [];
+  constructor( public http:HttpClient){
+    this.getFurnitureList();
+  }
+  getFurnitureList(){
+    this.http.get('assets/allFurnitures.json',  { observe: 'body' }).subscribe(result => {
+      console.log(result);
+      this.furnitureArr = result;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from  '@angular/common/http';
+import { NgxStarRatingModule } from 'ngx-star-rating';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxStarRatingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
